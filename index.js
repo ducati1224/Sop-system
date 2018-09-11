7ed21acbe1d7b4161f7ac86116c141354bd76840
Benjamin Wang <crocodile00323@gmail.com>
2018-09-11 18:05:19 +0800

    connect to sql & support file upload

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const sopRoutes = require('./routes/sop');
 // Upload setting
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // Routes
 app.use('/sop', sopRoutes)
--- routes/sop.js
+++ routes/sop.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const router = express.Router();
+
+// MSSQL
 const sql = require('mssql');
 const db = require('../config/config');
 
+// Handle upload file
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: function(req, file, cb){
+        cb(null, './upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now())
+    }
+})
+const upload = multer({
+    storage: storage
+})
+
+// sop Routes
 router.get('/', function(req, res){
     sql.connect(db, function(err){
         if (err) {
             console.log(err)
         }
         const request = new sql.Request();
-        // request.input('id', sql.NVarChar, '111111')
         request.query('select * from deviceSop', function(err, result){
             if (err) {
                 console.log(err)
                 res.send(err)
             }
             sql.close();
+            let flag = result.recordset.length;
             console.log('data send')
-            res.json(result.recordset);
+            if (flag) {
+                console.log('not null')
+            }
+            res.json(result);
         });
     })
 })
@@ -56,6 +76,25 @@ router.delete('/:id', function(req, res){
                    sql.close();
                    res.json(result.recordset);
                })
+    })
+})
+
+router.post('/test', upload.single('file'), function(req, res){
+    console.log('enter upload route');
+    sql.connect(db, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        const request = new sql.Request();
+        request.query('select * from deviceSop', function (err, result) {
+            if (err) {
+                console.log(err)
+                res.send(err)
+            }
+            sql.close();
+            console.log('data send')
+            res.json(result.recordset);
+        });
     })
 })
 
